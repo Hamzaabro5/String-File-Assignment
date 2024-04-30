c89e572f788b8f89d71e109c8cc7598261932c49
Hamzaabro5 <hamzaabro683@gmail.com>
2024-04-30 15:20:07 +0500

    String File

---
+++ app.js
@@ -0,0 +1,79 @@
+// console.log(`Hello String`);
+const h2 = document.querySelector(`h2`)
+
+
+
+// // Answer No 1
+
+// const firstName = prompt(`Enter Your First Name`)
+// const lastName = prompt(`Enter Your Last Name`)
+// const fullName = firstName + lastName
+
+
+
+// console.log(`Hello ${fullName}`);
+// h2.innerHTML = `Hello ${fullName}`
+
+// // Answer No 1
+
+
+
+
+
+
+
+
+
+// // Answer No 2
+
+// const mobileInput = prompt(`What is your favorite phone model?`)
+// h2.innerHTML = `My Favorite Phone is ${mobileInput} length of string : ${mobileInput.length} `
+// console.log(`My Favorite Phone is ${mobileInput} length of string : ${mobileInput.length} `);
+
+// // Answer No 2
+
+
+
+
+
+
+
+
+
+// // Answer No 3
+
+// const getString = prompt(`Enter Any Word That Contain Word n`)
+// console.log(`String: ${getString}`);
+// console.log(`Index of n is : ${getString.indexOf(`n`)}`);
+
+// // Answer No 3
+
+
+
+
+
+
+
+
+
+// // Answer No 4
+
+// const getString = prompt(`Enter Any Word That Contain Word L`)
+// console.log(`String: ${getString}`);
+// console.log(`Last index of L is : ${getString.lastIndexOf(`l`)}`);
+
+// // Answer No 4
+
+
+
+
+
+
+
+
+
+// Answer No 5
+
+
+
+// Answer No 5
